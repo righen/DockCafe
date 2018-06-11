fc37237fdedb6bcbe038fa60a8e633c5f5736382
righen sawmynaden <righensawmynaden@gmail.com>
2018-06-11 20:27:16 +0400

    Add project files.

---
+++ DockCafe/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DockCafe.MvcApplication" Language="C#" %>
---
+++ DockCafe/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DockCafe.Startup))]
+namespace DockCafe
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
